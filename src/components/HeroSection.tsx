fa08adb41e5635a776d1cb775c712d548ca0d3d5
muzammil shaheen <muzammilshaheen77@gmail.com>
2024-11-24 18:55:20 +0500

    s

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ function HeroSection() {
       <HeroHighlight>
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
+        fill="black"
       />
         <motion.h1
           initial={{
